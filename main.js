02de7719cc4a352390aa448183ba20d456ea3cce
Kalys01 <kalysosmon96.g@gmail.com>
2021-02-14 21:27:43 +0600

    include Vue and create style.css file

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-let gen = document.querySelector('h1');
-let btn = document.querySelector('.btn');
+const App = {
+    data() {
+        return {
+            title: '2',
+        }
+    },
 
-btn.addEventListener('click', generator);
-
-function generator(){
-    gen.innerHTML = Math.floor(Math.random() * 999);
 }
+
+Vue.createApp(App).mount('#app');
+
+
+// let gen = document.querySelector('h1');
+// let btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', generator);
+
+// function generator(){
+//     gen.innerHTML = Math.floor(Math.random() * 999);
+// }
 
 // console.log(Math.floor(Math.random() * 12));
